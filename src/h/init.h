0b2ea06e377e07c821629845c729a5e51f1525af
Jojo-A <33325587+Jojo-A@users.noreply.github.com>
2017-11-21 09:46:38 +0100

    Update init.h

--- src/h/init.h
+++ src/h/init.h
@@ -1,7 +1,6 @@
 #include <ESP8266WiFi.h>
 
-
-// инициализация платы ESP-01
+// card initialization ESP-01
 void initESP (void);
 bool loadConfig(String *ssid, String *pass);
 bool saveConfig(String *ssid, String *pass);
