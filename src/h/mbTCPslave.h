7d3320d0ed341321f055564419bfdcd8507a0c84
Jojo-A <33325587+Jojo-A@users.noreply.github.com>
2017-11-21 09:48:21 +0100

    Update mbTCPslave.h

--- src/h/mbTCPslave.h
+++ src/h/mbTCPslave.h
@@ -6,7 +6,6 @@
 #include <ESP8266WiFi.h>
 //#include "RtuMaster.h"
 
-
 #define MODBUSIP_MAXFRAME   200
 #define TCP_BUFFER_SIZE     300
 #define FRAME_COUNT         10
@@ -30,7 +29,7 @@ class cTcpSlave
     uint8_t nClient;
     uint8_t buffer[TCP_BUFFER_SIZE];
     uint16_t len;
-    uint32_t millis;                // Время отправки пакета в Serial
+    uint32_t millis;                // Time of sending the package to Serial
   };
 
   struct
@@ -48,8 +47,8 @@ class cTcpSlave
     uint8_t   _ui;  // Unit Identifier
   };
 void waitNewClient (void);  //
-void readDataClient(void);  // сканирование клиента на наличие данных
-void readFrameClient(WiFiClient  client, uint8_t i);   // разбор данных с клиента
+void readDataClient(void);  // customer scan for data availability
+void readFrameClient(WiFiClient  client, uint8_t i);   // parsing data from the client
 void writeFrameClient(void);
 void mbapUnpack (smbap* pmbap, uint8_t * buff );
 public:
