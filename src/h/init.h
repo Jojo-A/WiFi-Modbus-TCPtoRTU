f4aea070f07e2ead0cdc9d69db0027f97557c872
Jojo-A <33325587+Jojo-A@users.noreply.github.com>
2017-11-22 14:52:20 +0100

    Update init.h

--- src/h/init.h
+++ src/h/init.h
@@ -2,5 +2,5 @@
 
 // card initialization ESP-01
 void initESP (void);
-bool loadConfig(String *ssid, String *pass);
-bool saveConfig(String *ssid, String *pass);
+bool loadConfig(String *ssid, String *pass, String *name);
+bool saveConfig(String *ssid, String *pass, String *name);
